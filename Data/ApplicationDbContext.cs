67b9f8edff10762fe74827dd6bb5d0f8ac5001e7
Fredrick Njuguna <kfred45.fk@gmail.com>
2024-10-26 21:16:37 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using KiambuInternationalUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KiambuInternationalUniversity.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace KiambuInternationalUniversity.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime RegestrationDate { get; set; }
+        public bool Subsription { get; set; }
+    }
+}
